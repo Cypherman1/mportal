c2b0486c89e20848f7332ead6a50d382a94b1d8c
Cypherman1 <tungnt.finance@gmail.com>
2022-11-20 21:02:45 +0700

    build

---
+++ client_web/dist/precache-manifest.0f40709c0efefc2c22cb6cd3f3806766.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceab8d384666ca5b6474",
+    "url": "/css/about.7bdd8020.css"
+  },
+  {
+    "revision": "a946431faa1c50178eda",
+    "url": "/css/app.561ac4a8.css"
+  },
+  {
+    "revision": "508c9ef69bcbc5a6ab84",
+    "url": "/css/chunk-vendors.ec581cbb.css"
+  },
+  {
+    "revision": "c3a11ea5788cf6ba35bd829a214cbd24",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "2a61b00fcc9b1afbf99be145777b1b72",
+    "url": "/fonts/MaterialIcons-Regular.2a61b00f.eot"
+  },
+  {
+    "revision": "51cf1d641ae503ece727e7d84b182524",
+    "url": "/fonts/MaterialIcons-Regular.51cf1d64.ttf"
+  },
+  {
+    "revision": "703cf8f274fbb265d49c6262825780e1",
+    "url": "/fonts/MaterialIcons-Regular.703cf8f2.woff2"
+  },
+  {
+    "revision": "84a37de85c17f186652a179b1145392f",
+    "url": "/fonts/MaterialIcons-Regular.84a37de8.woff"
+  },
+  {
+    "revision": "099a9556e1a63ece24f8a99859c94c7d",
+    "url": "/fonts/fa-brands-400.099a9556.woff"
+  },
+  {
+    "revision": "30cc681d4487d2f561035ba24a68c629",
+    "url": "/fonts/fa-brands-400.30cc681d.eot"
+  },
+  {
+    "revision": "3b89dd103490708d19a95adcae52210e",
+    "url": "/fonts/fa-brands-400.3b89dd10.ttf"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "/fonts/fa-brands-400.f7307680.woff2"
+  },
+  {
+    "revision": "1f77739ca9ff2188b539c36f30ffa2be",
+    "url": "/fonts/fa-regular-400.1f77739c.ttf"
+  },
+  {
+    "revision": "7124eb50fc8227c78269f2d995637ff5",
+    "url": "/fonts/fa-regular-400.7124eb50.woff"
+  },
+  {
+    "revision": "7630483dd4b0c48639d2ac54a894b450",
+    "url": "/fonts/fa-regular-400.7630483d.eot"
+  },
+  {
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "/fonts/fa-regular-400.f0f82301.woff2"
+  },
+  {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "/fonts/fa-solid-900.1042e8ca.eot"
+  },
+  {
+    "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
+    "url": "/fonts/fa-solid-900.605ed792.ttf"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "/fonts/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "/fonts/fa-solid-900.e8a427e1.woff2"
+  },
+  {
+    "revision": "0cfbc3f0b079d4adb2ce5fa853487b73",
+    "url": "/img/Avatar0.0cfbc3f0.png"
+  },
+  {
+    "revision": "c21807c18416167be5bb2ab6c701773c",
+    "url": "/img/arg.c21807c1.png"
+  },
+  {
+    "revision": "122b9ebbf5be4562479a2a8468dc99f8",
+    "url": "/img/banner.122b9ebb.jpeg"
+  },
+  {
+    "revision": "1a4bbf5311506e37f5ae5f5759a1dff8",
+    "url": "/img/bra.1a4bbf53.png"
+  },
+  {
+    "revision": "18f5937075134bb83308c7feef0de312",
+    "url": "/img/ecu.18f59370.png"
+  },
+  {
+    "revision": "ba7ed552362f64d30f6d844974d89114",
+    "url": "/img/fa-brands-400.ba7ed552.svg"
+  },
+  {
+    "revision": "0bb428459c8ecfa61b22a03def1706e6",
+    "url": "/img/fa-regular-400.0bb42845.svg"
+  },
+  {
+    "revision": "376c1f97f6553dea1ca9b3f9081889bd",
+    "url": "/img/fa-solid-900.376c1f97.svg"
+  },
+  {
+    "revision": "0bd02249881564fc6aae83205a87b253",
+    "url": "/img/ira.0bd02249.png"
+  },
+  {
+    "revision": "6731c147b39a3b59824383691bfb3beb",
+    "url": "/img/kor.6731c147.png"
+  },
+  {
+    "revision": "3d2524d7a947b26eb0b7d8baf2d051dc",
+    "url": "/img/logo.3d2524d7.svg"
+  },
+  {
+    "revision": "6be51bfbf5f938d50ebc68366bb17ed4",
+    "url": "/img/por.6be51bfb.png"
+  },
+  {
+    "revision": "e865eca1da160b40397d4fb345368422",
+    "url": "/img/sau.e865eca1.png"
+  },
+  {
+    "revision": "c1a95626fe1974169ed2f0ce90132198",
+    "url": "/img/ser.c1a95626.png"
+  },
+  {
+    "revision": "22f5946edbba4ae769a4f5e37a6bd260",
+    "url": "/img/usa.22f5946e.png"
+  },
+  {
+    "revision": "79977b6e8b21e9796344a4336470d354",
+    "url": "/img/wal.79977b6e.png"
+  },
+  {
+    "revision": "3cf68982f4e188214f41c92832b723bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceab8d384666ca5b6474",
+    "url": "/js/about.b5a11da4.js"
+  },
+  {
+    "revision": "a946431faa1c50178eda",
+    "url": "/js/app.0df67b8f.js"
+  },
+  {
+    "revision": "508c9ef69bcbc5a6ab84",
+    "url": "/js/chunk-vendors.20b864a1.js"
+  },
+  {
+    "revision": "36a513f93fc108024380055e601a70a2",
+    "url": "/logo.png"
+  },
+  {
+    "revision": "b9e4853d2f1f90ff78ba9db8356b30f6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
